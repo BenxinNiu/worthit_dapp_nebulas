f7e0bea819bf29e7eea2266c7acd23320fdafe58
bn2645@mun.ca <bn2645@mun.ca>
2018-06-09 12:23:19 -0400

    deployed tested contract on test net and created tmp.js for info

---
+++ smartcontract/tmp.js
@@ -0,0 +1,23 @@
+var create_topic ={
+'date': "yyyy-MM-dd",
+'title':'test_topic',
+'tag':'test_tag',
+'worthit': 'true',
+'user_id': 'null',
+'user_addr': 'null',
+'comment': 'test_comment no comments',
+'upvote': '0',
+'downvote': '0',
+'id': 'null'};
+
+
+var createVote = {
+'id' : 1,
+'condititon'; "upvote or downvote"
+}
+
+var test_string = ["{\"title\":\"test_topic_1\",\"tag\":\"test_tag\",\"worthit\":\"true\",\"user_id\":\"null\",\"user_addr\":\"null\",\"comment\":\"test_comment no comments\",\"upvote\":\"0\",\"downvote\":\"0\",\"id\":\"null\"}"]
+var test_string = ["{\"id\":1, \"condition\": \"upvote\"}"]
+
+
+var stable_contract_addr_testnet = "n1h7EDDFG3DJ5tizZibHmwRiu4pq5i2UXwu"
