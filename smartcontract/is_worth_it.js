7ff07ac765bcf5e121f17475b8498627ff7936df
bn2645@mun.ca <bn2645@mun.ca>
2018-06-09 11:12:34 -0400

    updated new logic

--- smartcontract/is_worth_it.js
+++ smartcontract/is_worth_it.js
@@ -1,105 +1,55 @@
-// Atuhor Benxin Niu
-'use strict';
+
+'use strict' ;
 
 var is_worth_it = function(){
-  // key is the topic_id
-LocalContractStorage.defineMapProperty(this, "topics")
-//key is the address value is the user_id
-LocalContractStorage.defineMapProperty(this, "user") // used
-LocalContractStorage.defineProperty(this, "num_user") // used
-LocalContractStorage.defineProperty(this,"topic_bucket") // used
-LocalContractStorage.defineProperty(this, "tag_list") // used
+
+//LocalContractStorage.defineMapProperty(this, "topic_index");
+LocalContractStorage.defineMapProperty(this,"topic_list");
+LocalContractStorage.defineMapProperty(this, "user");
+// properties
+LocalContractStorage.defineProperty(this, "topice_list_index");
+LocalContractStorage.defineProperty(this, "topic_index");
+LocalContractStorage.defineProperty(this, "user_index");
+
 }
 
 is_worth_it.prototype = {
 
-  init: function(){
-    this.num_user = 0;
-    this.topic_bucket = [];
-    this.tag_list = [];
-  },
+init :function(){
+  this.user_index = 0;
+  this.topic_index = 0;
+  this.topice_list_index = 0;
+},
 
-  getTopicsCount: function(){
-    return this.topic_bucket.length;
-  },
-
-  getTagsCount: function(){
-    return this.tag_list.lenght;
-  },
-
-  getUserCount: function(){
-    return this.num_user;
-  },
-
-  createVote: function(req){
-    const from_address = Blockchain.transaction.from;
-    const json_req = JSON.parse(req);
-    _modifiy_topic(req.id,req.condition);
-  },
-
-  getTopics: function(){
-    const json_res = {
-      "tags": this.tag_list,
-      "topics": this.topic_bucket
-    }
-    return JSON.stringify(json_res);
-  },
-
-  createNewTopic: function(req){
-    var topic = _formatTopic(req);
-    _update_usr_topic_list(topic);
-    _update_topic_bucket(topic);
-  },
-
-  _modifiy_topic: function(id, condition){
-    var i;
-    for (i=0; i<this.topic_bucket.length;i++){
-      if(this.topic_bucket[i].id==id){
-      if(condition=="upvote"){
-        var up=parseInt(this.topic_bucket[i].upvote) + 1;
-        this.topic_bucket[i].upvote = up;
-      }
-      else{
-        var down=parseInt(this.topic_bucket[i].downvote) + 1;
-        this.topic_bucket[i].downvote = down;
-      }
-     break;
-    }
-    }
-  },
-
-  _update_topic_bucket: function(topic){
-    if(!this.tag_list.includes(topic.tag.toLowerCase()))
-      this.tag_list.push(topic.tag.toLowerCase());
-    this.topic_bucket.push(topic);
-  },
-
-  _update_usr_topic_list: function(topic){
-    var usr_topics = this.topics.get(topic.user_id);
-    if(!usr_topics){
-      this.topics.put(topic.user_id, [topic.id]);
-    }
-    else {
-      usr_topics.push(topic.id);
-      this.topics.delete(topic.user_id);
-      this.topics.put(topic.user_id, usr_topics);
-    }
-  },
-
-  _formatTopic: function(req){
-    const from_address = Blockchain.transaction.from;
-    var json = JSON.parse(req);
-    var usr = this.user.get(from_address);
-    if (!usr){
-      usr = this.num_user++;
-      this.user.put(from_address,usr);
-    }
-    json.user_id = usr;
-    json.user_addr = from_address;
-    json.id = this.topic_bucket.length;
-    return json;
+getTopics: function(){
+  var topics = new Array();
+  for(var i=0; i<=this.topic_index; i++){
+    var raw = this.topic_list.get(i);
+    topics.push(raw);
   }
+  var res = {data: topics};
+  return JSON.stringify(res);
+},
 
+createNewTopic: function(req){
+var json_req = JSON.parse(req);
+const from_addr = Blockchain.transaction.from;
+
+var usr_id = this.user.get(from_addr);
+if(!usr_id){
+  usr_id = this.user_index++;
+  this.user.put(from_addr,usr_id);
+}
+json_req.id = this.topic_index++;
+json_req.user_addr = from_addr;
+json_req.user_id = usr_id;
+
+this.topic_list.put(json_req.id, JSON.stringify(json_req));
+
+return "Topic created";
 
 }
-module.exports=is_worth_it;
+
+}
+
+module.exports = is_worth_it;
