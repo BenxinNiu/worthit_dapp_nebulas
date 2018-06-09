3cb666d2ad5d4e92ebc10a014d3254e4a3a9dfb0
bn2645@mun.ca <bn2645@mun.ca>
2018-06-09 17:25:21 -0400

    added search bar

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -50,8 +50,21 @@ app.controller('MainController',['$scope',function($scope){
     $scope.plusOne=function(index){
     $scope.products[index].likes += 1;
   };
-  
+
   $scope.minusOne=function(index){
     $scope.products[index].dislikes += 1;
   }
+  $scope.expand_bar=function(){
+$('#searchbar-input').animate({width: 'toggle'});
+$("#searchbar-icon").toggle();
+$("#searchbar-cross").toggle(500);
+$('#searchbar-submit').toggle(100);
+  }
+  $scope.hide_bar=function(){
+    $('#searchbar-input').animate({width: 'toggle'});
+    $("#searchbar-cross").toggle();
+    $('#searchbar-submit').toggle();
+    $("#searchbar-icon").toggle(500);
+  }
+
 }])
