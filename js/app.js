87e5725c2259bc776b436b28c5de7c8a52874e26
bn2645@mun.ca <bn2645@mun.ca>
2018-06-10 23:41:25 -0400

    added ui router

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-var app=angular.module("worthit_dapp",[]);
+var app=angular.module("worthit_dapp",['ui.router']);
+// app.config(function($stateProvider){
+//   $urlRouterProvider.otherwise('/')
+//   $stateProvider.state('landing', {
+//     url: '/',
+//     templateUrl : "views/landing.html",
+//     controller : 'mainController'
+//   })
+//   $stateProvider.state('newTopic',{
+//     templateUrl : "views/newTopics.html",
+//     controller : 'TopicsController'
+//   })
+// })
