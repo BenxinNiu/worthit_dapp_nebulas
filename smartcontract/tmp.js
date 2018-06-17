b7defe9fe1a9d645b561f1a3d74f29ca298fa219
benxin niu <bn2645@mun.ca>
2018-06-17 15:58:42 -0400

    updated tmp.js

--- smartcontract/tmp.js
+++ smartcontract/tmp.js
@@ -17,7 +17,7 @@ var createVote = {
 }
 
 var test_string = ["{\"title\":\"test_topic_1\",\"tag\":\"test_tag\",\"worthit\":\"true\",\"user_id\":\"null\",\"user_addr\":\"null\",\"comment\":\"test_comment no comments\",\"upvote\":\"0\",\"downvote\":\"0\",\"id\":\"null\"}"]
-var test_string = ["{\"id\":1, \"condition\": \"upvote\"}"]
+var test_string = ["{\"id\":1, \"condition\": \"upvote\"}"];
 
 
 var stable_contract_addr_testnet = "n1h7EDDFG3DJ5tizZibHmwRiu4pq5i2UXwu"
