5e0119ed8735021b8985e3d6ce46138550d73bcd
benxin niu <bn2645@mun.ca>
2018-06-17 12:15:27 -0400

    updated address testnet

--- js/services/sharedContractInfo.js
+++ js/services/sharedContractInfo.js
@@ -1,7 +1,7 @@
 app.factory('contractInfo', function(){
   var NebPay = require("nebpay");
   var nebPay = new NebPay();
-  const contract_address = "n1vnBqqkDx74aqtpdBexX1AKzHHbXpfpxTZ";
+  const contract_address = "n1pDAXD6ovZ9V1srHXxufCRfpMpkMJHuHmp";
   const callBackUrl = NebPay.config.testnetUrl;
 
     return {
@@ -10,6 +10,7 @@ app.factory('contractInfo', function(){
       nebPay : nebPay
     };
 
-})
+});
+//n1gGHow91fRHQdHk43mYe1URwTQZsfNzTC4
 //n1h7EDDFG3DJ5tizZibHmwRiu4pq5i2UXwu
 //n1vnBqqkDx74aqtpdBexX1AKzHHbXpfpxTZ
