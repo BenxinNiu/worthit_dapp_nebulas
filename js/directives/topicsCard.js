c1bb5df8e064df2b29a518357e7c54bb7a9d4801
benxin niu <bn2645@mun.ca>
2018-06-17 12:14:42 -0400

    bug fixes

--- js/directives/topicsCard.js
+++ js/directives/topicsCard.js
@@ -1,4 +1,5 @@
-app.directive('topicsCard', function(){
+app.directive('topicsCard', function(contractInfo){
+
   return {
     restrict: 'E',
     scope:{
@@ -6,12 +7,49 @@ app.directive('topicsCard', function(){
     },
     templateUrl: "js/directives/html/topicCard.html",
     link: function(scope, element, attrs) {
-      scope.plusOne=function(index){
-      this.info.likes += 1;
+
+    const onCreateVote = function(res){
+      if(typeof(res) === 'string' && res.startsWith("Error")){
+        alert("Ooops.. please try again later");
+      }
+      console.log(res);
     };
 
-    scope.minusOne=function(index){
-    this.info.dislikes += 1;
+
+
+    const submitVote = function(topic_id,type){
+        const contract_address = contractInfo.addr;
+        const callBackUrl = contractInfo.url;
+        const nebPay = contractInfo.nebPay;
+        // const req = {
+        //   id: topic_id.toString(),
+        //   condition: type
+        // };
+        // console.log(req);
+
+        nebPay.call(contract_address,0,"createVote",JSON.stringify([topic_id, type]),{
+            listener: onCreateVote
+        });
+    };
+
+
+
+      scope.voteUp=function(){
+      if(!this.info.upvote_clicked) {
+          this.info.upvote_clicked = true;
+          this.info.likes += 1;
+          submitVote(this.info.id, "upvote");
+      }
+    };
+
+
+
+    scope.voteDown=function(){
+    if(!this.info.downvote_clicked) {
+        this.info.downvote_clicked=true;
+        this.info.dislikes += 1;
+        submitVote(this.info.id, "downvote");
+    }
     };
 }
   };
